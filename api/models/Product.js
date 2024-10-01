f05626dd8abf5afc77609217bad7bafef4b4ecd7
Mordecai Chingolo <138593277+Mord100@users.noreply.github.com>
2024-10-01 14:35:43 +0200

    Update Product.js

--- api/models/Product.js
+++ api/models/Product.js
@@ -16,7 +16,7 @@ const prodcutSchema = new mongoose.Schema({
   yearOfMake: { type: String },
   rating: { type: Number, required: true, default: 0 },
   numReview: { type: Number, required: true, default: 0 },
-  price: { type: Number, required: true, default: 0 },
+  price: { type: String, required: true, default: 0 },
   countInStock: { type: Number, required: true, default: 0 },
 
   // reviews: [reviewSchema],
