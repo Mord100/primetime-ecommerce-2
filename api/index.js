36d94a676890e0d20883bd267c493b64befab82c
Mordecai <chingolo265@gmail.com>
2024-10-17 12:08:12 +0200

    new server updates

--- api/index.js
+++ api/index.js
@@ -1,70 +1,51 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const colors = require('colors')
-const cors = require("cors")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const cors = require("cors");
+const path = require('path');
 
-
-const products = require('./data/products')
-
-const connectDB = require('./config/db')
-
-
-const port= process.env.PORT || 5001
-
-connectDB()
-
-const app = express()
-
+const connectDB = require('./config/db');
 const databaseSeeder = require("./databaseSeeder");
 const userRoute = require("./routes/User");
 const productRoute = require("./routes/Product");
 const orderRoute = require("./routes/Order");
 const cartRoute = require("./routes/Cart");
 
+const port = process.env.PORT || 5001;
 
+connectDB();
 
-app.use(express.json())
+const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+// Middlewares
+app.use(express.json());
+app.use(cors({ origin: '*' }));
 
-//database seeder routes
+// API Routes
 app.use("/api/seed", databaseSeeder);
-
-//routes for users
 app.use("/api/users", userRoute);
-
-//routes for products
 app.use("/api/products", productRoute);
-
-//routes for orders
 app.use("/api/orders", orderRoute);
-
-//routes for cart
 app.use("/api/cart", cartRoute);
 
-// paypal payment api for client key;
-app.use("/api/config/paypal", (req, res) => {
+// PayPal API Key Route
+app.get("/api/config/paypal", (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
-  });
-// Import the path module
-const path = require('path');
-
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// // Catch-all route - this should be the last route
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-// });
-
-app.listen(port, () => console.log(`server started on port ${port}`))
-
-app.get('*', (req, res) => {
-  res.status(404).send('Not Found');
 });
 
+// Static Files
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+// Catch-All Route for React
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
+
+// 404 Handler
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 module.exports = app;
-
